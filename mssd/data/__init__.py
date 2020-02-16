b53a9dc59f30f1bd4ced46c32a99de41a5834bef
nakamunya <itonari.nakamura@gmail.com>
2020-02-16 13:55:16 +0900

    add custom collate_fn

--- mssd/data/__init__.py
+++ mssd/data/__init__.py
@@ -1,10 +1,29 @@
 from torch.utils.data import DataLoader
+from torch.utils.data.dataloader import default_collate
 # from torchvision.transforms import Compose, ToTensor, Normalize
 # from torchvision.datasets import MNIST
 
 from mssd.data.transforms import build_transforms, build_target_transform
 from mssd.data.datasets import build_dataset
+from mssd.structures.container import Container
 
+class BatchCollator:
+    def __init__(self, is_train=True):
+        self.is_train = is_train
+
+    def __call__(self, batch):
+        transposed_batch = list(zip(*batch))
+        images = default_collate(transposed_batch[0])
+        img_ids = default_collate(transposed_batch[2])
+
+        if self.is_train:
+            list_targets = transposed_batch[1]
+            targets = Container(
+                {key: default_collate([d[key] for d in list_targets]) for key in list_targets[0]}
+            )
+        else:
+            targets = None
+        return images, targets, img_ids
 
 def get_data_loaders(cfg):
     train_transform = build_transforms(cfg, is_train=True)
@@ -20,12 +39,14 @@ def get_data_loaders(cfg):
     train_loader = DataLoader(train_dataset,
                                 batch_size=cfg["training"]["batch_size"],
                                 shuffle=True,
-                                num_workers=cfg["data_loader"]["num_workers"])
+                                num_workers=cfg["data_loader"]["num_workers"],
+                                collate_fn=BatchCollator(is_train=True))
     
     validate_loader = DataLoader(validate_dataset,
                                 batch_size=cfg["validate"]["batch_size"],
                                 shuffle=False,
-                                num_workers=cfg["data_loader"]["num_workers"])
+                                num_workers=cfg["data_loader"]["num_workers"],
+                                collate_fn=BatchCollator(is_train=False))
 
     # data_transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
     # train_loader = DataLoader(MNIST(download=True, root=".", transform=data_transform, train=True),
